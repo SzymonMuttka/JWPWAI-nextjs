0b111bb2dd1a7b89a839173a4ccf7a2cacfe4aaf
SzymonMuttka <21278@student.ans-elblag.pl>
2025-12-01 11:33:57 +0100

    Lab8 cały

--- Lab7/app/meals/page.js
+++ Lab7/app/meals/page.js
@@ -1,6 +1,28 @@
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+
 export default function Meals() {
     return (
-        <h1>Meals</h1>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favourite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your favourite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     );
   }
   
