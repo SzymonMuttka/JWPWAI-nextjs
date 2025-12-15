92817c4d9a5502f377ae569dffd524aa86b970d9
SzymonMuttka <21278@student.ans-elblag.pl>
2025-12-15 11:38:44 +0100

    Zamiana bazy danych na Supabase

--- Lab7/lib/meals.js
+++ Lab7/lib/meals.js
@@ -1,14 +1,29 @@
-import sql from 'better-sqlite3';
+//import sql from 'better-sqlite3';
+import { supabase } from './supabase';
 
-const db = sql('meals.db');
+//const db = sql('meals.db');
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
+    const { data, error } = await supabase.from('meals').select('*');
+
+    console.log('data: ', data);
+
+    if(error){
+        throw error;
+    }
+    return data;
+
     //throw new Error('Loading meals failed');
-    return db.prepare('SELECT * FROM meals').all();
+    //return db.prepare('SELECT * FROM meals').all();
 }
 
-export function getMeal(slug) {
-    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+export async function getMeal(slug) {
+    const { data, error } = await supabase.from('meals').select('*').eq('slug', slug).single();
+    if(error){
+        throw error;
+    }
+    return data;
+    //return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
